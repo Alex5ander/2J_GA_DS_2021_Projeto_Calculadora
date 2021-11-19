489ecc85863cf168b61966c435e4096833b4a464
Alex5ander <alexsandergutierreez@gmail.com>
2021-11-19 19:59:09 -0300

    initial

--- ProjetoCalculadora/Form1.Designer.cs
+++ ProjetoCalculadora/Form1.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnTopo = new System.Windows.Forms.Panel();
+            this.pnCentral = new System.Windows.Forms.Panel();
+            this.lbDisplay = new System.Windows.Forms.Label();
+            this.pn = new System.Windows.Forms.Panel();
+            this.pnTopo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnTopo
+            // 
+            this.pnTopo.BackColor = System.Drawing.Color.Silver;
+            this.pnTopo.Controls.Add(this.lbDisplay);
+            this.pnTopo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnTopo.Location = new System.Drawing.Point(0, 0);
+            this.pnTopo.Margin = new System.Windows.Forms.Padding(0);
+            this.pnTopo.Name = "pnTopo";
+            this.pnTopo.Padding = new System.Windows.Forms.Padding(8);
+            this.pnTopo.Size = new System.Drawing.Size(434, 84);
+            this.pnTopo.TabIndex = 0;
+            // 
+            // pnCentral
+            // 
+            this.pnCentral.BackColor = System.Drawing.Color.Silver;
+            this.pnCentral.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnCentral.Location = new System.Drawing.Point(0, 84);
+            this.pnCentral.Name = "pnCentral";
+            this.pnCentral.Size = new System.Drawing.Size(321, 677);
+            this.pnCentral.TabIndex = 1;
+            // 
+            // lbDisplay
+            // 
+            this.lbDisplay.BackColor = System.Drawing.Color.Gainsboro;
+            this.lbDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbDisplay.Font = new System.Drawing.Font("Quartz MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDisplay.Location = new System.Drawing.Point(8, 8);
+            this.lbDisplay.Name = "lbDisplay";
+            this.lbDisplay.Size = new System.Drawing.Size(418, 68);
+            this.lbDisplay.TabIndex = 0;
+            this.lbDisplay.Text = "0";
+            this.lbDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pn
+            // 
+            this.pn.BackColor = System.Drawing.Color.Silver;
+            this.pn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pn.Location = new System.Drawing.Point(321, 84);
+            this.pn.Name = "pn";
+            this.pn.Size = new System.Drawing.Size(113, 677);
+            this.pn.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(434, 761);
+            this.Controls.Add(this.pn);
+            this.Controls.Add(this.pnCentral);
+            this.Controls.Add(this.pnTopo);
+            this.Font = new System.Drawing.Font("Quartz MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnTopo.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnTopo;
+        private System.Windows.Forms.Label lbDisplay;
+        private System.Windows.Forms.Panel pnCentral;
+        private System.Windows.Forms.Panel pn;
     }
 }
 
--- ProjetoCalculadora/Form1.cs
+++ ProjetoCalculadora/Form1.cs
@@ -16,5 +16,35 @@ namespace ProjetoCalculadora
         {
             InitializeComponent();
         }
+        //btnCalcular
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int size = Math.Min(pnCentral.Width, pnCentral.Height) / 3;
+            int left = 0;
+            int top = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Button button = new Button();
+                button.Text = (9 - i).ToString();
+                button.Name = "btn" + (9 - i).ToString();
+                button.Width = size;
+                button.Height = size;
+                button.BackColor = Color.White;
+                button.Cursor = "Hand";
+
+                if (i % 3 == 0 && i != 0)
+                {
+                    left = 0;
+                    top += size; 
+                }
+
+                button.Left = left;
+                button.Top = top;
+
+                left += size;
+                
+                pnCentral.Controls.Add(button);
+            }
+        }
     }
 }
